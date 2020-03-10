a894e2977141fc4b04ca699dd03de22a0b1b23e1
Hung Q. Vu <cvuhu@jci.com>
2020-03-10 10:20:40 -0500

    FIRST UPDATE

---
+++ ConsoleHostApp/ConsoleHostApp/BlockingCollectionQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleHostApp
+{
+
+   public class BlockingCollectionQueue
+
+    {
+
+        private BlockingCollection<object> _jobs= new BlockingCollection<object>();
+
+
+        public BlockingCollectionQueue()
+
+{
+
+            var thread = new Thread(new ThreadStart(OnStart));
+
+            thread.IsBackground = true;
+
+            thread.Start();
+
+        }
+
+
+        public void Enqueue(object job)
+
+        {
+
+            _jobs.Add(job);
+
+        }
+
+
+        private void OnStart()
+
+        { 
+
+            foreach (var job in _jobs.GetConsumingEnumerable(CancellationToken.None))
+
+            {
+
+                Console.WriteLine(job);
+
+            }
+
+        }
+
+    }
+
+
+}
